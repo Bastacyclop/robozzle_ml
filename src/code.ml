b7fea74335e41bb424008a2412c092160b134e40
Thomas Koehler <basta.t.k+git@gmail.com>
2015-05-01 01:11:20 +0200

    Dirty Tail Call optimization

--- src/code.ml
+++ src/code.ml
@@ -21,9 +21,17 @@ type precompiled_instruction =
     | Rotate of Puzzle.rotation
     | Color of Puzzle.color
     | Call of symbol
+    | TailCall of symbol
     | Return
     | Jump of label
     | JumpIfNot of Puzzle.color * label
+
+let rec find_forward label pre_instrs =
+    match pre_instrs with
+    | [] -> None
+    | (Label label')::t -> if label = label' then Some t
+                                             else None
+    | h::t -> find_forward label t
 
 let compile prog =
     let instr_count = ref 0 in
@@ -59,10 +67,71 @@ let compile prog =
         in
         Return::res
     in
+    let rev_tail_call_optimization pre_def =
+        let known_color = ref None in
+        let is_possible color =
+            match !known_color with
+            | None -> true
+            | Some c -> color = c
+        in
+        let rec is_tail_call l =
+            let try_follow label l =
+                match find_forward label l with
+                | Some t -> is_tail_call t
+                | None -> false (* We don't handle this case *)
+            in
+            match l with
+            | [] -> failwith "unreachable"
+            | h::t -> (
+                match h with
+                | Return -> true
+                | Label _ -> is_tail_call t
+                | Jump label -> try_follow label t
+                | JumpIfNot (color, label) ->
+                    if is_possible color then is_tail_call t
+                    else try_follow label t
+                | _ -> false
+            )
+        in
+        let color_end_label = ref None in
+        let rec iter l acc =
+            match l with
+            | [] -> acc
+            | (Call symbol)::t ->
+                let acc =
+                    if is_tail_call t then (TailCall symbol)::acc
+                                      else     (Call symbol)::acc
+                in
+                iter t acc
+            | h::t -> (
+                match h with
+                | JumpIfNot (color, label) -> (
+                    known_color := Some color;
+                    color_end_label := Some label;
+                )
+                | Label label -> (
+                    match !color_end_label with
+                    | Some c_e_l ->
+                        if label = c_e_l then (
+                            known_color := None;
+                            color_end_label := None;
+                        )
+                    | None -> ()
+                )
+                | _ -> ();
+                ;
+                iter t (h::acc)
+            )
+        in
+        iter pre_def []
+    in
     let precompile prog =
         let Program defs = prog in
         List.fold_left
-            (fun acc d -> (List.rev (rev_precompile_definition d))::acc)
+            (fun acc d -> (d
+                |> rev_precompile_definition |> List.rev
+                |> rev_tail_call_optimization |> List.rev)::acc
+            )
             []
             defs
         |> List.rev
@@ -91,6 +160,7 @@ let compile prog =
         | Rotate r -> Rotate r
         | Color c -> Color c
         | Call symbol -> Call (LabelMap.find symbol labels)
+        | TailCall symbol -> TailCall (LabelMap.find symbol labels)
         | Return -> Return
         | Jump label -> Jump (LabelMap.find label labels)
         | JumpIfNot (color, label) -> JumpIfNot (color, LabelMap.find label labels)
